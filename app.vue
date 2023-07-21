9527ba2b860c41c8d70e4610eb1e9a7a8a1c7583
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-07-21 10:11:44 +0300

    Change color of text on main page for unauth user

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
       class="d-flex justify-center align-center"
       style="min-height: 100vh;"
     >
-      <span class="text-blue text-2xl">
+      <span class="text-primary text-h4">
         Только для авторизованных пользователей
       </span>
     </div>
