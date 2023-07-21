e1850ef7c30619e4f1ffebebe208b5d37172a242
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-07-21 11:20:19 +0300

    Breadcrumbs with navigation at footer

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage v-if="canRenderPages" />
-    <div
-      v-else
-      class="d-flex justify-center align-center"
-      style="min-height: 100vh;"
-    >
-      <span class="text-primary text-h4">
-        Только для авторизованных пользователей
-      </span>
-    </div>
-  </NuxtLayout>
+  <v-app>
+    <NuxtLayout>
+      <NuxtPage v-if="canRenderPages" />
+      <div
+        v-else
+        class="d-flex justify-center align-center"
+        style="min-height: 100vh;"
+      >
+        <span class="text-primary text-h4">
+          Только для авторизованных пользователей
+        </span>
+      </div>
+    </NuxtLayout>
+    <Breadcrumbs />
+  </v-app>
 </template>
 <script setup lang="ts">
 import Routes from '~/constants/Routes';
--- constants/Routes.ts
+++ constants/Routes.ts
@@ -1,3 +1,10 @@
+const INDEX = { path: '/', title: 'Главная' };
+const LOGIN = { path: '/login', title: 'Логин' };
+const ALL = [INDEX, LOGIN];
+export function getRouteTitleByPath(path: String) {
+  return ALL.find(r => r.path === path)?.title;
+}
 export default {
-  LOGIN: { path: '/login' },
+  INDEX,
+  LOGIN,
 };
