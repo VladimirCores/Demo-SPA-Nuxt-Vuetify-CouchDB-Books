63a365033e02a442a7341cd61b9fd8e37466d4b7
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-07-21 10:12:32 +0300

    Change default 'light' theme primary and secondary colors.

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,11 +4,21 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { md2 } from 'vuetify/blueprints';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import colors from 'vuetify/lib/util/colors';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     theme: {
       defaultTheme: 'light',
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.blue.darken1,
+            secondary: colors.blue.lighten4,
+          },
+        },
+      },
     },
     blueprint: md2,
     icons: {
